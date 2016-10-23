ed9246ad4670b92a33d3515d631e885f2f59f12b
shavit <shavit@users.noreply.github.com>
2016-10-23 18:03:23 -0400

    🛥

---
+++ lib/path_finder/astar.ex
@@ -0,0 +1,19 @@
+defmodule PathFinder.AStar do
+  def path(item) do
+    # Find neighbors
+
+    # Find not visited
+
+    # Set next
+    item
+  end
+
+  def neighbors(item) do
+  end
+  
+end
+
+queue = []
+visited = []
+
+Enum.map(queue, &(PathFinder.AStar.path(&1)))
